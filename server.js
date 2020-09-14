103699866f9dc0ac4985eab61c1792804710e2fe
Jot <70845012+JotOye@users.noreply.github.com>
2020-09-14 12:26:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const Bot = require("ctk-musicbot");
+
+const musicbot = new Bot({
+  Token: "NzQyMzk2NzE1NjI5NTQzNDk1.XzFg0g.RGXFfXMBgRQGxDetuBpCi0c1qBA",
+  prefix: "#"
+});
+
+musicbot.start();
+
